6ab7bd066d6ce1200d8847aee219c00446c9ba10
Geoff <geoffrymichael@gmail.com>
2018-02-01 03:26:27 +0000

    add further base npm packages to setup automation, but we are now at the meat and potatoes. First css modification done to the site in centering banner text in the banner image.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require("gulp");
 var watch = require("gulp-watch");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var cssvars = require("postcss-simple-vars");
+var nested = require("postcss-nested");
+var cssImport = require("postcss-import");
 
 gulp.task("default", function() {
    console.log("please work"); 
@@ -10,7 +15,9 @@ gulp.task("html", function() {
 });
 
 gulp.task("styles", function() {
-   console.log("imagine sass or post css stuff here"); 
+   return gulp.src("./app/assets/styles/styles.css")
+      .pipe(postcss([cssImport, cssvars,nested, autoprefixer]))
+      .pipe(gulp.dest("./app/temp/styles"));
 });
 
 gulp.task("watch", function() {
