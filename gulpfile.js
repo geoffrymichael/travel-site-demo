717c6467bf2fbc5bbabda6b68668dd3c92aa24bd
Geoff <geoffrymichael@gmail.com>
2018-02-01 06:45:06 +0000

    split gulpfile into multiple files for organization. configure styles.js gulp task to not stop gulp watch on error and instead output the error info to the console

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,8 @@
-var gulp = require("gulp");
-var watch = require("gulp-watch");
-var postcss = require("gulp-postcss");
-var autoprefixer = require("autoprefixer");
-var cssvars = require("postcss-simple-vars");
-var nested = require("postcss-nested");
-var cssImport = require("postcss-import");
-var browserSync = require("browser-sync").create();
+require("./gulp/tasks/styles");
+require("./gulp/tasks/watch");
 
-gulp.task("default", function() {
-   console.log("please work"); 
-});
 
-gulp.task("html", function() {
-   console.log("do some html stuff"); 
-});
 
-gulp.task("styles", function() {
-   return gulp.src("./app/assets/styles/styles.css")
-      .pipe(postcss([cssImport, cssvars,nested, autoprefixer]))
-      .pipe(gulp.dest("./app/temp/styles"));
-});
 
-gulp.task("watch", function() {
-   
-   browserSync.init({
-      server: {
-         baseDir: "app"
-      }
-   });
-   
-   watch("./app/index.html", function() {
-       browserSync.reload();
-   }); 
-   
-   watch("./app/assets/styles/**/*.css", function() {
-       gulp.start("cssInject");
-   })
-   
-});
 
-gulp.task("cssInject", ["styles"], function () {
-   return gulp.src(".app/temp/styles/styles.css")
-      .pipe(browserSync.stream());
-});
+
