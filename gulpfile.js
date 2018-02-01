ec50a9286fa89a56cf475606243d47f7222aa201
Geoff <geoffrymichael@gmail.com>
2018-02-01 06:15:42 +0000

    add gulp through npm install, require and configure

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var autoprefixer = require("autoprefixer");
 var cssvars = require("postcss-simple-vars");
 var nested = require("postcss-nested");
 var cssImport = require("postcss-import");
+var browserSync = require("browser-sync").create();
 
 gulp.task("default", function() {
    console.log("please work"); 
@@ -22,13 +23,23 @@ gulp.task("styles", function() {
 
 gulp.task("watch", function() {
    
+   browserSync.init({
+      server: {
+         baseDir: "app"
+      }
+   });
+   
    watch("./app/index.html", function() {
-       gulp.start("html");
+       browserSync.reload();
    }); 
    
    watch("./app/assets/styles/**/*.css", function() {
-       gulp.start("styles");
+       gulp.start("cssInject");
    })
    
 });
 
+gulp.task("cssInject", ["styles"], function () {
+   return gulp.src(".app/temp/styles/styles.css")
+      .pipe(browserSync.stream());
+});
