8a5235f1b15940b0e5ea1b8d28f9808ee1d45c1d
Geoff <geoffrymichael@gmail.com>
2018-02-20 04:04:57 +0000

    Add support for browser fallback

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -2,15 +2,28 @@ var gulp = require("gulp");
 var svgSprite = require("gulp-svg-sprite");
 var rename = require("gulp-rename");
 var del = require("del");
+var svg2png =require("gulp-svg2png");
 
 var config = {
+    shape: {
+        spacing: {
+            padding: 1
+        }
+    },
     mode: {
         css: {
-          sprite: "sprite.svg",
-          render: {
-              css: {
-                  template: "./gulp/templates/sprite.css"
-              }
+            variables: {
+                replaceSvgWithPng: function() {
+                    return function(sprite, render) {
+                        return render(sprite).split(".svg").join(".png");
+                    }
+                }
+            },
+            sprite: "sprite.svg",
+            render: {
+                css: {
+                    template: "./gulp/templates/sprite.css"
+                }
           }  
         }
     }
@@ -27,8 +40,14 @@ gulp.task("createSprite", ["beginClean"], function() {
     .pipe(gulp.dest("./app/temp/sprite/"));
 });
 
-gulp.task("copySpriteGraphic", ["createSprite"], function() {
-   return gulp.src("./app/temp/sprite/css/**/*.svg")
+gulp.task("createPngCopy", ["createSprite"], function() {
+   return gulp.src("./app/temp/sprite/css/*.svg")
+    .pipe(svg2png())
+    .pipe(gulp.dest("./app/temp/sprite/css"));
+});
+
+gulp.task("copySpriteGraphic", ["createPngCopy"], function() {
+   return gulp.src("./app/temp/sprite/css/**/*.{svg,png}")
     .pipe(gulp.dest("./app/assets/images/sprites"));
 });
 
@@ -42,4 +61,4 @@ gulp.task("endClean", ["copySpriteGraphic", "copySpriteCSS"], function() {
    return del("./app/temp/sprite"); 
 });
 
-gulp.task("icons", ["beginClean", "createSprite", "copySpriteGraphic", "copySpriteCSS", "endClean"]);
+gulp.task("icons", ["beginClean", "createSprite", "createPngCopy", "copySpriteGraphic", "copySpriteCSS", "endClean"]);
